5d15ffcd8e3626b66237857d91b008687de46a11
Unknown <chriswayg@gmail.com>
2018-05-02 14:25:16 +0800

    Add optional RainLoop webmail install

---
+++ files/vars.local.inc.php
@@ -0,0 +1,11 @@
+<?php
+$MAILCOW_APPS = array(
+  array(
+    'name' => 'RainLoop Webmail',
+    'link' => '/webmail/'
+   ),
+  array(
+    'name' => 'SOGo Groupware',
+    'link' => '/SOGo/'
+  )
+);
